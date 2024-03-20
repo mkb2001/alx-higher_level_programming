e1c32652024ec40512686a8e0fb69e8d81782d35
mkb2001 <kevinmugarura051@gmail.com>
2024-03-20 17:33:58 +0300

    Change mode of files

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    max_integer = 0
-    for i in range (len(my_list) - 1):
-        if max_integer < my_list[i]:
-            max_integer = my_list[i]
-    return max_integer
+    if len(my_list) == 0:
+        return "None"
+    else:
+        max = my_list[0]
+        for i in range(len(my_list)):
+            if my_list[i] > max:
+                max = my_list[i]
+        return max
