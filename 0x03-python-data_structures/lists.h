b2997c6dee186cc64f5ca76093ff4c5c140d8246
mkb2001 <kevinmugarura051@gmail.com>
2024-03-20 17:19:57 +0300

    Add 3 files

--- 0x03-python-data_structures/lists.h
+++ 0x03-python-data_structures/lists.h
@@ -1,3 +1,4 @@
+
 #ifndef LISTS_H
 #define LISTS_H
 
@@ -7,21 +8,20 @@
  * @next: points to the next node
  *
  * Description: singly linked list node structure
- *
+ * for project
  */
+#include <stdlib.h>
+#include <stdio.h>
 typedef struct listint_s
 {
     int n;
     struct listint_s *next;
 } listint_t;
 
-#include <stdlib.h>
-#include <stdio.h>
 size_t print_listint(const listint_t *h);
 listint_t *add_nodeint_end(listint_t **head, const int n);
 void free_listint(listint_t *head);
-void reverse_listint(listint_t **head);
 
-listint_t *insert_node(listint_t **head, int number);
+int is_palindrome(listint_t **head);
 
 #endif /* LISTS_H */
