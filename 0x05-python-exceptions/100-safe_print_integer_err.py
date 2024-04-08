822f5f5f090495f856367482eaadaf8e92b772f8
mkb2001 <kevinmugarura051@gmail.com>
2024-04-08 07:11:41 +0300

    Add safe_print_integer_err

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -3,6 +3,6 @@ def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
         return True
-    except ValueError as e:
+    except TypeError as e:
         print("Exception: Unknown format code 'd' for object of type 'str' School is not an integer")
         return False
