fad91fdde300f222f36de0a0f926668ebdb6ae4a
mkb2001 <kevinmugarura051@gmail.com>
2024-04-08 17:41:06 +0300

    Add Square class

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -48,7 +48,7 @@ class Square:
 
     @position.setter
     def position(self, value):
-        if isinstance(value, tuple):
+        if isinstance(value, tuple) or isinstance(value[0], int) or isinstance(value[1], int):
             if len(value) == 2:
                 self.__position = value
             else:
