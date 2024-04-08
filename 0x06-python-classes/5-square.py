6828681d5c539f77e67786c402b57eb4c2d2e012
mkb2001 <kevinmugarura051@gmail.com>
2024-04-08 15:19:54 +0300

    Add Square class

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -38,4 +38,10 @@ class Square:
     """This Function prints the class"""
 
     def my_print(self):
-        print(self.__str__())
+        if self.__size == 0:
+            print()
+        else:
+            for i in range(0, self.__size):
+                for j in range(0, self.__size):
+                    print('#', end="")
+                print()
