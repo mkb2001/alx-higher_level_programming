b18cca0b57ea1f8fcc517eca3ea31d926d1e3f25
mkb2001 <kevinmugarura051@gmail.com>
2024-04-08 14:12:28 +0300

    Add Square class

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 """ This class is a Square class"""
 
+
 class Square:
     """ This is the first method in the class to be run at initialization"""
+
     def __init__(self, size):
         self.__size = size
