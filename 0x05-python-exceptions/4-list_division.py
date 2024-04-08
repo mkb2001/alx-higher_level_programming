f87fecfc74c330f608ab3e8203cfb35408b651d3
mkb2001 <kevinmugarura051@gmail.com>
2024-04-08 06:40:30 +0300

    Add raise_exception

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -19,6 +19,6 @@ def list_division(my_list_1, my_list_2, list_length):
                 new_list.append(c)
 
     except IndexError:
-        print("out of range")
+        pass
     return new_list
 
