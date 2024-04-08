dfed2c5ce94e6a20851029c0b5954ad36c5ddb90
mkb2001 <kevinmugarura051@gmail.com>
2024-04-08 07:31:34 +0300

    Add safe_function

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
+from __future__ import print_function
 import sys
 
 
 def safe_function(fct, *args):
     try:
-        x, y = args
-        res = fct(x, y)
-    except Exception as err:
-        print("Exception:", err, file=sys.stderr)
+        res = fct(*args)
+    except Exception as e:
+        print("Exception: {}".format(e), file=sys.stderr)
         return None
     else:
         return res
