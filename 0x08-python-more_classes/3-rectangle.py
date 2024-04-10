c1c68e6c97452ae973aecc8c12e9709e431edb3e
mkb2001 <kevinmugarura051@gmail.com>
2024-04-11 00:22:08 +0300

    Add 3-rectangle.py

---
+++ 0x08-python-more_classes/3-rectangle.py
@@ -0,0 +1,67 @@
+#!/usr/bin/python3
+"""This is a module representation of a rectangle i Python"""
+
+
+class Rectangle:
+    """This is a class representation of a rectangle i Python"""
+
+    def __init__(self, width=0, height=0):
+        if not isinstance(width, int) or not isinstance(height, int):
+            raise TypeError("width must be an integer")
+        if width < 0 or height < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = width
+        self.__height = height
+
+    @property
+    def width(self):
+        return (self.__width)
+
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        return (self.__height)
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        return self.__height * self.__width
+
+    def perimeter(self):
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        return (2 * (self.__width + self.__height))
+
+    def print(self):
+        """Prints the current state of the"""
+        if self.__width == 0 or self.__height == 0:
+            print("")
+        for i in range(self.__width):
+            for j in range(self.__height):
+                print("#", end="")
+            print()
+
+    def str(self):
+        """Returns the shape of the object"""
+        res = ""
+        if self.__width == 0 or self.__height == 0:
+            print("")
+        for i in range(self.__width):
+            for j in range(self.__height):
+                res += '#'
+            # print()
+            res += '\n'
+        return res
