8373016a6c1fc748cbbca02c034967aaf5971a7a
mkb2001 <kevinmugarura051@gmail.com>
2024-04-11 00:31:41 +0300

    Add 3-rectangle.py corrections

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -54,16 +54,14 @@ class Rectangle:
                 print("#", end="")
             print()
 
-    def str(self):
-        """Return the printable representation of the Rectangle.
-
-                Represents the rectangle with the # character.
-                """
+    def __str__(self):
+        """Returns the shape of the object"""
+        res = ""
         if self.__width == 0 or self.__height == 0:
-            return ("")
-        rect = []
-        for i in range(self.__height):
-            [rect.append('#') for j in range(self.__width)]
-            if i != self.__height - 1:
-                rect.append("\n")
-        return ("".join(rect))
+            print("")
+        for i in range(self.__width):
+            for j in range(self.__height):
+                res += '#'
+            # print()
+            res += '\n'
+        return res
