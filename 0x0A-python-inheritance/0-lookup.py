e15ceaf4c0c0fc82c2e9ff31af59b09853a277d9
mkb2001 <kevinmugarura051@gmail.com>
2024-04-20 12:26:12 +0300

    ADD LOOKUP

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,11 @@
+def lookup(obj):
+    """
+        Return the list of available attributes and methods of an object.
+
+        Args:
+        - obj: Any Python object
+
+        Returns:
+        - List of strings representing attributes and methods
+        """
+    return dir(obj)
