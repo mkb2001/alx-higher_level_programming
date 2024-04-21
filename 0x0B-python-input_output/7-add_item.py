4ac5e9543012b2401a8148361877b1aaad0ba47d
mkb2001 <kevinmugarura051@gmail.com>
2024-04-22 00:31:57 +0300

    ADD add_item

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python3
 """
-Add all arguments to a Python list, and then save them to a file
+Write a script that adds all arguments to a Python list, and then save them to a file:
+
+You must use your function save_to_json_file from 5-save_to_json_file.py
+You must use your function load_from_json_file from 6-load_from_json_file.py
+The list must be saved as a JSON representation in a file named add_item.json
+If the file doesn’t exist, it should be created
+You don’t need to manage file permissions / exceptions.
 """
 
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
@@ -8,10 +14,10 @@ load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 
 def add_item(args):
-    """Add all arguments to a Python list, and then save them to a file"""
     try:
         my_list = load_from_json_file("add_item.json")
-    except (FileNotFoundError, ValueError):
+    except:
         my_list = []
-    my_list.extend(args)
+    for arg in args:
+        my_list.append(arg)
     save_to_json_file(my_list, "add_item.json")
