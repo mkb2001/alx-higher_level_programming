3ce98229bddaeb231d18e5faaf18f003077aec95
mkb2001 <kevinmugarura051@gmail.com>
2024-04-22 00:30:46 +0300

    ADD add_item

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
-save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 """
 Add all arguments to a Python list, and then save them to a file
 """
+
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 
 def add_item(args):
