52ade1e5f03cc37de441a2b8b2623e72577bd8ac
mkb2001 <kevinmugarura051@gmail.com>
2024-04-22 22:30:54 +0300

    change base.py

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -19,3 +19,13 @@ class Base:
         if list_dictionaries is None or len(list_dictionaries) == 0:
             return '[]'
         return str(list_dictionaries)
+
+    def save_to_file(cls, list_objs):
+        """ Writes the JSON string representation of list_objs to a file """
+        filename = cls.__name__ + '.json'
+        with open(filename, 'w') as file:
+            if list_objs is None:
+                file.write('[]')
+            else:
+                list_dicts = [obj.to_dictionary() for obj in list_objs]
+                file.write(cls.to_json_string(list_dicts))
