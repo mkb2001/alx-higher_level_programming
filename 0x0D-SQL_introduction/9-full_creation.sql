4eb130be0f607bf2670d46e6c70d776538bdad6a
Kevin Mugarura B <kevinmugarura051@gmail.com>
2024-05-27 15:44:06 +0300

    Refactor SQL script to improve table creation in second_table

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,5 +1,4 @@
 -- This is a script to create a table in a database
-CREATE TABLE IF NOT EXISTS second_table (id INT, name VARCHAR(256), score INT, checked BOOLEAN); INSERT INTO second_table (id, name, score, checked) VALUES (1, 'John', 10),
-    (2, 'Alex', 3),
-    (3, 'Bob', 14),
-    (4, 'George', 8);
+CREATE TABLE IF NOT EXISTS second_table (id INT, name VARCHAR(256), score INT);
+INSERT INTO second_table (`id`, `name`, `score`) VALUES (1, 'John', 10), (2, 'Alex', 3), (3, 'Bob', 14), (4, 'George', 8);
+
