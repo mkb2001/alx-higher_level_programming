94b32bdcf75f3aef3b8c83e22a94c9b9ad40554b
Kevin Mugarura B <kevinmugarura051@gmail.com>
2024-05-27 16:57:25 +0300

    Refactor SQL scripts to add new queries and improve table creation

--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -1,6 +1,7 @@
--- Lists all records of the table second_table having a name value in my MySQL server.
--- Records are ordered by descending score.
-SELECT `score`, `name`
-FROM `second_table`
-WHERE `name` != ""
-ORDER BY `score` DESC
+-- lists all shows, and all genres linked to that show, from the database hbtn_0d_tvshows
+-- lists all rows of a table linked to another table
+SELECT title, name
+FROM tv_shows
+LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+LEFT JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
+ORDER BY title ASC, name ASC;
