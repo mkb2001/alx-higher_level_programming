0b2c61528e761897d3a6cb8bce4f4d7b502d01fd
Kevin Mugarura B <kevinmugarura051@gmail.com>
2024-05-27 15:42:27 +0300

    Refactor SQL script to show full description of first_table

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,17 +1,3 @@
 -- This is  a script that prints the full description of the table first_table from the database hbtn_0c_0 in your MySQL server.
-SELECT 
-  COLUMN_NAME, 
-  DATA_TYPE, 
-  CHARACTER_MAXIMUM_LENGTH, 
-  NUMERIC_PRECISION, 
-  NUMERIC_SCALE, 
-  COLUMN_DEFAULT, 
-  IS_NULLABLE, 
-  COLUMN_KEY, 
-  EXTRA
-FROM 
-  INFORMATION_SCHEMA.COLUMNS 
-WHERE 
-  TABLE_NAME = 'first_table' 
-AND 
-  TABLE_SCHEMA = '$DB_NAME';
+-- shows tables in a database passed as an argument from tty
+SHOW CREATE TABLE first_table;
