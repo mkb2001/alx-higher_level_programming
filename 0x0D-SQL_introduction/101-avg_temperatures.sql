7745d78be1f07a6fceaa5034d708f9c5c72864ef
Kevin Mugarura B <kevinmugarura051@gmail.com>
2024-05-27 15:59:34 +0300

    Refactor SQL script to improve table creation in second_table

--- 0x0D-SQL_introduction/101-avg_temperatures.sql
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -1,3 +1,5 @@
 -- run with command:
-SOURCE temperatures.sql;
-SELECT `city`, AVG(`value`) AS avg_temp FROM temperatures GROUP BY `city` ORDER BY `avg_temp` DESC;
+SELECT `city`, AVG(`value`) AS `avg_temp`
+FROM `temperatures`
+GROUP BY `city`
+ORDER BY `avg_temp` DESC;
