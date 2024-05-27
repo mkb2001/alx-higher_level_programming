86cbbb2243f17f6969ca434be9eb56ae5086e343
Kevin Mugarura B <kevinmugarura051@gmail.com>
2024-05-27 15:58:08 +0300

    Refactor SQL script to convert database encoding to UTF8

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,4 +1,4 @@
 -- this is a script that converts the encoding of a database to UTF8
-ALTER DATABASE hbtn_0c_0 CHARACTER  SET = utfmb4 COLLATE = utfmb4_unicode_ci;
-ALTER DATABASE hbtn_0c_0.first_table COLLATE utf8mb4_unicode_ci;
-ALTER DATABASE hbtn_0c_0.first_table MODIFY COLUMN 'name' VARCHAR(256) COLLATE utf8mb4_unicode_ci
+USE `hbtn_0c_0`
+ALTER TABLE `first_table`
+CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
