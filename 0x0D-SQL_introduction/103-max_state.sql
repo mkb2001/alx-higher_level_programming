dbec25251488a72c0244a058449a99a4e3203fde
Kevin Mugarura B <kevinmugarura051@gmail.com>
2024-05-27 16:02:36 +0300

    Refactor SQL script to select maximum temperature by state

--- 0x0D-SQL_introduction/103-max_state.sql
+++ 0x0D-SQL_introduction/103-max_state.sql
@@ -1,6 +1,4 @@
-SELECT `city`, AVG(`value`) AS `avg_temp`
+SELECT `state`, MAX(`value`) AS `max_temp`
 FROM `temperatures`
-WHERE `month` = 7 OR `month` = 8
-GROUP BY `city`
-ORDER BY `avg_temp` DESC
-LIMIT 3;
+GROUP BY `state`
+ORDER BY `state`;
