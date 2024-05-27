560c45c100eb684db87c7dc3d9f1a55e03ec4b2f
Kevin Mugarura B <kevinmugarura051@gmail.com>
2024-05-27 16:53:15 +0300

    Refactor SQL scripts to add new queries and improve table creation

--- 0x0E-SQL_more_queries/100-not_my_genres.sql
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -1,4 +1,12 @@
--- Converts the entire database hbtn_0c_0 to UTF8.
-USE `hbtn_0c_0`
-ALTER TABLE `first_table`
-CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+-- uses the hbtn_0d_tvshows database to list all genres not linked to the show Dexter
+-- uses a database to list all rows not linked to one row
+SELECT name
+FROM tv_genres
+WHERE name NOT IN
+(SELECT name
+FROM tv_genres
+LEFT JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
+LEFT JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
+WHERE tv_shows.title = 'Dexter')
+GROUP BY name
+ORDER BY name ASC;
