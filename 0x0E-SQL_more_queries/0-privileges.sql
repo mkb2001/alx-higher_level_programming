760169b8ef41c48309cf68be9ae4166d14bfd26b
Kevin Mugarura B <kevinmugarura051@gmail.com>
2024-05-27 16:27:36 +0300

    Refactor SQL script to add privileges query and update README

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,2 +1,3 @@
 -- this is a script about privileges
-SELECT * FROM information_schema.user_privileges WHERE grantee IN ('user_0d_1@\'%\'', 'user_0d_2@\'%\'');
+SHOW GRANTS FOR user_0d_1@localhost;
+SHOW GRANTS FOR user_od_2@localhost;
