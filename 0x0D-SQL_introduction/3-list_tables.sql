afe335e229d0784d64b9434f58206373e1d125d4
Kevin Mugarura B <kevinmugarura051@gmail.com>
2024-05-27 13:23:25 +0300

    Refactor SQL script to list tables in MySQL

--- 0x0D-SQL_introduction/3-list_tables.sql
+++ 0x0D-SQL_introduction/3-list_tables.sql
@@ -1,3 +1,3 @@
---This is a script that lists all tables of a database in MySQL
---The database name will be passed as an argument of mysql command
+-- This is a script that lists all tables of a database in MySQL
+-- The database name will be passed as an argument of mysql command
 SHOW TABLES;
